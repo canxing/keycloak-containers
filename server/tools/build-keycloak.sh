1b9919cc9eda066bf3db32f31ed865a00140093e
liuxin <liuxincuit@163.com>
2024-07-17 09:17:49 +0800

    update build-keycloak.sh and docker-publish.yml

--- server/tools/build-keycloak.sh
+++ server/tools/build-keycloak.sh
@@ -40,7 +40,7 @@ if [ "$GIT_REPO" != "" ]; then
     # MASTER_HEAD=`git log -n1 --format="%H"`
     # echo "Keycloak from [build]: $GIT_REPO/$GIT_BRANCH/commit/$MASTER_HEAD"
 
-    # $M2_HOME/bin/mvn -Dmaven.test.skip clean install
+    $M2_HOME/bin/mvn -Dmaven.test.skip clean install
 
     # cd distribution
     # $M2_HOME/bin/mvn -Dmaven.test.skip clean install
@@ -48,24 +48,18 @@ if [ "$GIT_REPO" != "" ]; then
 
     $M2_HOME/bin/mvn -Pdistribution -pl distribution/server-dist -am -Dmaven.test.skip clean install
     
-    echo "maven install finish"
     cd /opt/jboss
-    echo "cd /opt/jboss"
 
     tar xfz /opt/jboss/keycloak-source/distribution/server-dist/target/keycloak-*.tar.gz
-    echo "tar xfz"
 
-    rm -rf /opt/jboss/keycloak-source
-    echo "rm -rf /opt/jboss/keycloak-source"
+    mv -rf /opt/jboss/keycloak-source /opt/jboss/source
     
     mv /opt/jboss/keycloak-* /opt/jboss/keycloak
-    echo "mv keycloak file"
+    ls /opt/jboss/keycloak/modules/system/layers/base/org/apache/cxf/*/*
 
     # Remove temporary files
     rm -rf /opt/jboss/maven
-    echo "rm -rf /opt/jboss/maven"
     rm -rf $HOME/.m2/repository
-    echo "rm -rf $HOME/.m2/repository"
 else
     echo "Keycloak from [download]: $KEYCLOAK_DIST"
 
@@ -78,31 +72,26 @@ fi
 # Create DB modules #
 #####################
 
-echo "mysql modules"
 mkdir -p /opt/jboss/keycloak/modules/system/layers/base/com/mysql/jdbc/main
 cd /opt/jboss/keycloak/modules/system/layers/base/com/mysql/jdbc/main
 curl -O https://repo1.maven.org/maven2/mysql/mysql-connector-java/$JDBC_MYSQL_VERSION/mysql-connector-java-$JDBC_MYSQL_VERSION.jar
 cp /opt/jboss/tools/databases/mysql/module.xml .
 sed "s/JDBC_MYSQL_VERSION/$JDBC_MYSQL_VERSION/" /opt/jboss/tools/databases/mysql/module.xml > module.xml
 
-echo "postgresql modules"
 mkdir -p /opt/jboss/keycloak/modules/system/layers/base/org/postgresql/jdbc/main
 cd /opt/jboss/keycloak/modules/system/layers/base/org/postgresql/jdbc/main
 curl -L https://repo1.maven.org/maven2/org/postgresql/postgresql/$JDBC_POSTGRES_VERSION/postgresql-$JDBC_POSTGRES_VERSION.jar > postgres-jdbc.jar
 cp /opt/jboss/tools/databases/postgres/module.xml .
 
-echo "mariadb modules"
 mkdir -p /opt/jboss/keycloak/modules/system/layers/base/org/mariadb/jdbc/main
 cd /opt/jboss/keycloak/modules/system/layers/base/org/mariadb/jdbc/main
 curl -L https://repo1.maven.org/maven2/org/mariadb/jdbc/mariadb-java-client/$JDBC_MARIADB_VERSION/mariadb-java-client-$JDBC_MARIADB_VERSION.jar > mariadb-jdbc.jar
 cp /opt/jboss/tools/databases/mariadb/module.xml .
 
-echo "oracle modules"
 mkdir -p /opt/jboss/keycloak/modules/system/layers/base/com/oracle/jdbc/main
 cd /opt/jboss/keycloak/modules/system/layers/base/com/oracle/jdbc/main
 cp /opt/jboss/tools/databases/oracle/module.xml .
 
-echo "sqlserver modules"
 mkdir -p /opt/jboss/keycloak/modules/system/layers/keycloak/com/microsoft/sqlserver/jdbc/main
 cd /opt/jboss/keycloak/modules/system/layers/keycloak/com/microsoft/sqlserver/jdbc/main
 curl -L https://repo1.maven.org/maven2/com/microsoft/sqlserver/mssql-jdbc/$JDBC_MSSQL_VERSION/mssql-jdbc-$JDBC_MSSQL_VERSION.jar > mssql-jdbc.jar
@@ -114,11 +103,9 @@ cp /opt/jboss/tools/databases/mssql/module.xml .
 
 cd /opt/jboss/keycloak
 
-echo "configure keycloak standalone"
 bin/jboss-cli.sh --file=/opt/jboss/tools/cli/standalone-configuration.cli
 rm -rf /opt/jboss/keycloak/standalone/configuration/standalone_xml_history
 
-echo "configure keycloak standalone-ha"
 bin/jboss-cli.sh --file=/opt/jboss/tools/cli/standalone-ha-configuration.cli
 rm -rf /opt/jboss/keycloak/standalone/configuration/standalone_xml_history
 
